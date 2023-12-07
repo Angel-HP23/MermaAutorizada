cc3b60584549af057e05ae2245313b883d7b0971
LaloST107 <eadweard132@gmail.com>
2023-12-07 15:11:43 -0600

    Se agrega: controllers para poder obtener limitesMerma, para eliminarLimiteMerma por Id, para insertar un nuevo limite, además de los metodos con sus filtros para hacer las operaciones, en el modelo se agrego la secuencia de la base de datos OT correspondiente a la tabla de MAP_LIMITES_MERMA para poder generar un ID autoincrementable  por ultimo las interfaces que complementan las clases

--- mermaautorizada/src/main/java/com/femsa/oxxo/mermaautorizada/request/InsertLimiteRequestImpl.java
+++ mermaautorizada/src/main/java/com/femsa/oxxo/mermaautorizada/request/InsertLimiteRequestImpl.java
@@ -1,4 +1,26 @@
 package com.femsa.oxxo.mermaautorizada.request;
 
-public class InsertLimiteRequestImpl {
+import com.femsa.oxxo.mermaautorizada.repository.XxmapLimitesMermaRepository;
+import lombok.Data;
+
+@Data
+public class InsertLimiteRequestImpl extends InsertLimiteRequest  {
+
+    public InsertLimiteRequestImpl() {
+
+    }
+
+
+    public InsertLimiteRequestImpl(InsertLimiteRequest insertLimiteRequest){
+
+
+        super.setRangoInicial(insertLimiteRequest.getRangoInicial());
+        super.setRangoFinal(insertLimiteRequest.getRangoFinal());
+        super.setMermaAutorizada(insertLimiteRequest.getMermaAutorizada());
+        super.setCreatedBy(insertLimiteRequest.getCreatedBy());
+        super.setCreationDate(insertLimiteRequest.getCreationDate());
+        super.setModifiedBy(insertLimiteRequest.getModifiedBy());
+        super.setModifiedDate(insertLimiteRequest.getModifiedDate());
+
+    }
 }
